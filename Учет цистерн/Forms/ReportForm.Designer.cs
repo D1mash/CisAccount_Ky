bde84e3b6bbc1c8df60b4c8c6d4539058b457acb
Dinmukamed <omarbaev95@mail.ru>
2019-09-16 17:50:40 +0600

    Добавлена папка Forms

--- Учет цистерн/Forms/ReportForm.Designer.cs
+++ Учет цистерн/Forms/ReportForm.Designer.cs
@@ -36,6 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "ReportForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Отчеты: Обработанные вагоны";
             this.ResumeLayout(false);
 
