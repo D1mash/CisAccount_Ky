acc8163a5074411764343406cac0a36a9d7e7316
Dinmukamed <omarbaev95@mail.ru>
2020-02-27 14:58:35 +0600

    Доработал

--- Учет цистерн/Forms/Логин/LoginForm.Designer.cs
+++ Учет цистерн/Forms/Логин/LoginForm.Designer.cs
@@ -74,6 +74,7 @@
             // 
             this.comboBox1.BackColor = System.Drawing.Color.White;
             this.comboBox1.DisplayMember = "FIO";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.DropDownWidth = 184;
             this.comboBox1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.comboBox1.ForeColor = System.Drawing.SystemColors.ControlText;
